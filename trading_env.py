1ed87c0b3d8c65151f1693be67daef9db695dbd7
Madusha <madusha.20200573@iit.ac.lk>
2022-03-04 15:42:26 +0530

    added three methods

--- trading_env.py
+++ trading_env.py
@@ -1,2 +1,12 @@
 class TradingEnvironment:
-    
+    def _init_(self, balance, symbol):
+        self.balance = balance
+        self.symbol = symbol
+        self.stock_buys = []
+        self.stock_sells = []
+
+    def buy(self, symbol, buy_price, time):
+        pass
+
+    def sell(self, sell_price, time):
+        pass
