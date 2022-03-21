a98d420539138f8883e63b7ee431ba94af430464
Hisham Bawa <hishambawa@icloud.com>
2022-03-21 13:59:34 +0530

    Added graph scripts and history

--- script.js
+++ script.js
@@ -1,26 +1,159 @@
+let finished = false;
+let buys = [];
+let sells = [];
+
+let balance = 1000;
+let bot_funds = 2000;
+
+let API_URL = "http://172.20.10.7:5000/";
+// let API_URL = "http://192.168.1.4:5000/";
+
 window.addEventListener("load", function() {
 
-    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+    var xValues = ["Start"];
+    var yValues = [bot_funds];
 
-    new Chart("chart", {
-    type: "line",
-    data: {
-        labels: xValues,
-        datasets: [
-        {
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgb(0, 0, 0)",
-            borderColor: "rgba(108, 122, 137, 0.5)",
-            data: yValues
-        }]
-    },
-    options: {
-        legend: {display: false},
-        scales: {
-        yAxes: [{ticks: {min: 6, max:16}}],
+    var chart = new Chart("chart", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [
+            {
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgb(0, 0, 0)",
+                // borderColor: "rgba(108, 122, 137, 0.5)",
+                borderColor: "#aaa",
+                data: yValues
+            }]
+        },
+        options: {
+            legend: {display: false},
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // min: 100, 
+                        // max:3000,
+                        fontColor: "#ccc"
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontColor: "#ccc"
+                    }
+                }]
+            }
         }
-    }
     });
-})
+    
+    startBot();
+    
+    let timer = setInterval(function() {
+
+        if(finished) {
+            clearInterval(timer);
+            console.log("finished");
+            console.log(buys);
+            console.log(sells);
+        } 
+
+        else {
+            getData(chart);
+        }
+        
+    }, 1000);
+
+});
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function startBot() {
+
+    console.log("Bot Start")
+
+    let data =
+    {
+        balance: balance,
+        symbols: ["A", "AAPL", "GOOG", "FB"]
+    }
+
+    fetch(API_URL + 'start', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        })
+        .then(response => {
+            finished = true;
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+let bought = false;
+
+function getData(chart) {
+
+    fetch(API_URL + 'get')
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(json => {
+
+        if(!bought && buys.length != json.buys.length) {
+            buys.push(json.buys[json.buys.length - 1]);
+
+            bot_funds = bot_funds - json.buys[json.buys.length - 1][2]
+
+            addData(chart, json.buys[json.buys.length - 1][1], bot_funds);
+
+            console.log(json.buys[json.buys.length - 1], balance, "buy");
+
+            bought = true;
+
+            let template =
+                `<tr>
+                    <td>`+ json.buys[json.buys.length - 1][1] +`</td>
+                    <td>USD `+ json.buys[json.buys.length - 1][3] +`</td>
+                    <td>`+ json.buys[json.buys.length - 1][4] +`</td>
+                    <td>BUY</td>
+                </tr>`;
+
+            document.getElementById("history-table").innerHTML += template;
+
+        }
+
+        if(bought && sells.length != json.sells.length) {
+            sells.push(json.sells[json.sells.length - 1]);
+
+            bot_funds = bot_funds + json.sells[json.sells.length - 1][2]
+
+            addData(chart, json.sells[json.sells.length - 1][1], bot_funds);
+
+            console.log(json.sells[json.sells.length - 1], balance, "sell");
+
+            bought = false;
+
+            let template =
+                `<tr>
+                    <td>`+ json.sells[json.sells.length - 1][1] +`</td>
+                    <td>USD `+ json.sells[json.sells.length - 1][3] +`</td>
+                    <td>`+ json.sells[json.sells.length - 1][4] +`</td>
+                    <td>SELL</td>
+                </tr>`;
+
+            document.getElementById("history-table").innerHTML += template;
+
+        }
+
+    })    
+    .catch(err => console.log('Request Failed', err)); // Catch errors
+
+}
