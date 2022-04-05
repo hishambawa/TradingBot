dfb38a1cd23765086abb1bf79b23d0375e0c8376
Hisham Bawa <hishambawa@icloud.com>
2022-04-05 15:48:01 +0530

    Added static url path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 
 from bot import Bot
 
-app = Flask(__name__, static_folder='frontend')
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app) # Allow cross origin requests
 
 users = {}
